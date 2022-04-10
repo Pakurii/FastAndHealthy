26af754dc5adec9c1ca1e5afe4c86126150b4ba4
Ahmet Borslan <borslanahmet@gmail.com>
2022-04-10 15:49:52 +0300

    deneme

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {Text,View } from 'react-native';
+import React from 'react';
+const App=()=>
+{
+  return(
+    <View>
+      <Text>sdrdsrtHi</Text>
+    </View>
+  )
+}
+export default App;
