b07fbbbc1ef547ac3f03910cbfae39ee09eca0fd
DogukanSakin <dogukansakin1@hotmail.com>
2022-05-06 20:19:06 +0300

    Login Page tasarım hatasının giderilmesi

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './src/Pages/Login Page/';
-import App from "./src/Pages/Register Slider"
+import App from "./src/Pages/Login Page"
 import {name as appName} from './app.json';
 AppRegistry.registerComponent(appName, () => App);
--- src/Pages/Login Page/LoginPage.js
+++ src/Pages/Login Page/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text,View,SafeAreaView,Image,ImageBackground} from 'react-native';
+import { Text,View,SafeAreaView,Image,ImageBackground,Dimensions} from 'react-native';
 import styles from './LoginPageStyle';
 import Logo from '../../Components/Logo';
 import InputBox from '../../Components/InputBox';
@@ -8,33 +8,34 @@ import Button from '../../Components/Button/Button';
 const LoginPage=()=>{
     return(
         <SafeAreaView style={styles.container}>
-       
                 <Logo/>
-                <InputBox iconName={'account-outline'} placeholder="Kullanıcı Adı"/>
-                <InputBox  iconName={'lock-outline'} placeholder="Şifre" isPassword={true}/>
-                <View style={styles.innerContainer}>
-                
-                
-                    <Text style={styles.rememberMeText}>Beni Hatırla</Text>
-                    <Text style={styles.forgotPasswordText}>Şifreni mi unuttun?</Text>
+                <View style={styles.centerContainer}>
+                    <InputBox iconName={'account-outline'} placeholder="Kullanıcı Adı"/>
+                    <InputBox  iconName={'lock-outline'} placeholder="Şifre" isPassword={true}/>
+                    <View style={styles.rememberAndForgotPasswordContainer}>
+                        <Text style={styles.rememberMeText}>Beni Hatırla</Text>
+                        <Text style={styles.forgotPasswordText}>Şifreni mi unuttun?</Text>
+                    </View>
+                   <Button buttonText={'Giriş Yap'}/>
+                    
+                    <View style={styles.dontHaveAccountContainer}>
+                        <Text style={styles.dontHaveAccountText}>Bir hesabın yok mu? </Text>
+                        <Text style={styles.registerText}>Kayıt Ol!</Text>
+                        
+                    </View>
+                    <View style={styles.bottomContainer}>
+                        
+                    <ImageBackground style={styles.bottomBackgroundImage} source={require('../../../assets/images/loginFooterImage.png')}>
+                        
+                        <Image style={styles.bottomImage} source={require('../../../assets/images/Subtract.png')}></Image>
+                        
+                    </ImageBackground>
+                    </View>
+                    
+                    
                 </View>
-                <Button buttonText={'Giriş Yap'}/>
-                <View style={styles.dontHaveAccountContainer}>
-                    <Text style={styles.dontHaveAccountText}>Bir hesabın yok mu? </Text>
-                    <Text style={styles.registerText}>Kayıt Ol!</Text>
-                </View>
                 
-                <ImageBackground style={styles.loginFooterImage} source={require('../../../assets/images/loginFooterImage.png')}>
-                
-                    <Image style={styles.loginFooterTopImage} source={require('../../../assets/images/loginFooterImageBackground.png')}></Image>
-                    <Text style={styles.copyrightFooterText}>HealthyTech ©</Text>
-               
-                
-                </ImageBackground>
-                
-                
-          
-      
+    
             
         </SafeAreaView>
     )
