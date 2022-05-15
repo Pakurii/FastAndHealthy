463525aee052e91db93e59d3bdfa7920b95b71a2
Ahmet Borslan <borslanahmet@gmail.com>
2022-05-15 15:00:07 +0300

    Repair

--- index.js
+++ index.js
@@ -7,7 +7,5 @@ import {AppRegistry} from 'react-native';
 
 import App from './src/Pages/Register Slider';
 
-import App from "./src/Pages/Login Page"
-
 import {name as appName} from './app.json';
 AppRegistry.registerComponent(appName, () => App);
